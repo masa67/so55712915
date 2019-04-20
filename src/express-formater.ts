45fa8a9b339c2d02544319392984bc6d8d302d3b
Matti Sangi <adm.masaweb@gmail.com>
2019-04-20 09:12:47 +0300

    Simplify

--- src/express-formater.ts
+++ src/express-formater.ts
@@ -1,11 +1,10 @@
 
-import core from 'express-serve-static-core';
+import 'express-serve-static-core';
 import express from 'express';
 
 declare module 'express-serve-static-core' {
   export interface Response {
-    respondWith: (p1: any) => Response,
-    test: () => void
+    respondWith: (p1: any) => Response
   }
 }
 
@@ -15,9 +14,5 @@ express.response.respondWith = function(data: string) {
     data: data
   });
 };
-
-express.response.test = function() {
-  console.log('this is working...');
-}
 
 export default express;
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import express from "./express-formater";
 const app = express();
 const port = 8080;
 
-express.response.test();
-
 app.get("/", (req, res) => res.respondWith({ ok: true }) );
 
 app.listen(port, () =>
